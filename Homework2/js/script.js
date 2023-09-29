9d95758d320447914b965cf320079139db2b410e
Casey Dockery <cbdocke2@ncsu.edu>
2023-09-28 21:07:45 -0400

    Added javascript and added baseline functionality for calculations

--- Homework2/js/script.js
+++ Homework2/js/script.js
@@ -1,156 +1,227 @@
+let textbox = "";
+let beforeOperator = true;
+
+let equation = {
+    'firstValue': 0,
+    'secondValue': 0,
+    operator: 'notset'
+};
+
+
+function evaluateEquation() {
+    if (equation.operator == "+") {
+        return equation.firstValue + equation.secondValue;
+    }
+    if (equation.operator == "-") {
+        return equation.firstValue - equation.secondValue;
+    }
+    if (equation.operator == "*") {
+        return equation.firstValue * equation.secondValue;
+    }
+    if (equation.operator == "/") {
+        return equation.firstValue / equation.secondValue;
+    }
+}
 document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#zero");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("0");
+        // const textnode = document.createTextNode("0");
         
-        document.querySelector("#textbox").appendChild(textnode);
+
+        // document.querySelector("#textbox").appendChild(textnode);
+        textbox += "0";
+        
+        document.querySelector("#textbox").innerHTML = textbox;
+        
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#one");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("1");
+        textbox += "1";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#two");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("2");
+        textbox += "2";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#three");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("3");
+        textbox += "3";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#four");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("4");
+        textbox += "4";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#five");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("5");
+        textbox += "5";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#six");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("6");
+        textbox += "6";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#seven");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("7");
+        textbox += "7";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#eight");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("8");
+        textbox += "8";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#nine");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("9");
+        textbox += "9";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#decimal");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode(".");
+        textbox += ".";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#plussign");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("+");
+        textbox += "+";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#minussign");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("-");
+        textbox += "-";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#Xsign");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("X");
+        textbox += "*";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#slash");
     btn.addEventListener('click', (e) => {
-        const textnode = document.createTextNode("/");
+        textbox += "/";
         
-        document.querySelector("#textbox").appendChild(textnode);
+        document.querySelector("#textbox").innerHTML = textbox;
+    });
+   });
+
+   document.addEventListener('DOMContentLoaded', (e) => {
+    let btn = document.querySelector("#clear");
+    btn.addEventListener('click', (e) => {
+        textbox = "";
+        document.querySelector("#textbox").innerHTML = textbox;
     });
    });
 
    document.addEventListener('DOMContentLoaded', (e) => {
     document.addEventListener('keydown', (e) => {
         if (e.key == "Backspace") {
-            console.log(document.querySelector("#textbox").childNodes.length);
-            if (document.querySelector("#textbox").childNodes.length != 0) {
-                document.querySelector("#textbox").removeChild(document.querySelector("#textbox").lastChild);
-            }
+            textbox = textbox.substring(0, textbox.length - 1);
+            document.querySelector("#textbox").innerHTML = textbox;
         }
         else {
             const isDigit = parseInt(e.key);
-            console.log(isDigit);
-            if (!isNaN(isDigit) || e.key == "+" || e.key=="-" || e.key=="/" ||  e.key==".") {
-                const textnode = document.createTextNode(e.key);
-                document.querySelector("#textbox").appendChild(textnode);
+            if (e.key == "+" || e.key=="-" || e.key=="/" ||  e.key==".") {                
+                if (beforeOperator) {
+                    equation.firstValue = parseInt(textbox);
+                    beforeOperator = false;
+                    textbox= "";
+                }
+                else if (!beforeOperator) {
+                    equation.secondValue = parseInt(textbox);
+                    beforeOperator = true;
+                }
+                textbox += e.key;
+                equation.operator = e.key;
+                document.querySelector("#textbox").innerHTML = textbox;
+            }
+            else if (e.key == "Enter") {
+                if (!beforeOperator) {
+                    textbox = textbox.substring(1, textbox.length);
+                    equation.secondValue = parseInt(textbox);
+                    console.log(equation.firstValue);
+                    console.log(equation.secondValue);
+                    beforeOperator = true;
+                    textbox = evaluateEquation();
+                    document.querySelector("#textbox").innerHTML = textbox;
+                }
+            }
+            else if (!isNaN(isDigit)) {
+                textbox += e.key;
+                
+                document.querySelector("#textbox").innerHTML = textbox;
             }
             else if(e.key == "*" || e.key == "x" || e.key == "X") {
-                const textnode = document.createTextNode("*");
-                document.querySelector("#textbox").appendChild(textnode);
+                if (beforeOperator) {
+                    equation.firstValue = parseInt(textbox);
+                    beforeOperator = false;
+                    textbox= "";
+                }
+                else if (!beforeOperator) {
+                    equation.secondValue = parseInt(textbox);
+                    beforeOperator = true;
+                }
+                textbox += "*";
+                equation.operator = "*";
+                document.querySelector("#textbox").innerHTML = textbox;
+                
             }
         }
     })

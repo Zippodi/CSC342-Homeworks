02fb1ae0af19a3e746ba116ed781386854897d8c
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-22 18:41:21 -0400

    Changed the base route to /api

--- Homework4/src/routes.js
+++ Homework4/src/routes.js
@@ -6,14 +6,16 @@ let followers = require('./data/follows.json');
 let users = require('./data/users.json');
 
 
-router.get('/howls', (req, res) => {
+router.get('/api/howls', (req, res) => {
   res.json(howls);
 });
 
-router.get('/follows', (req, res) => {
+router.get('/api/follows', (req, res) => {
   res.json(followers);
 });
 
-router.get('/users', (req, res) => {
+router.get('/api/users', (req, res) => {
   res.json(users);
 });
+
+module.exports = router;

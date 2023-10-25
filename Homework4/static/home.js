2c4c7c440f923e74b0e3f29d664d5c112460961c
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-25 17:29:53 -0400

    Logging In seems to work now

--- Homework4/static/home.js
+++ Homework4/static/home.js
@@ -2,8 +2,11 @@ import api from './APIClient.js';
 
 
 const loggedInUser = localStorage.getItem('user');
+const profilePicture = document.querySelector('#profilePic');
 const username = document.querySelector('#Username');
 // username.innerHTML = loggedInUser.username;
-username.innerHTML = loggedInUser;
+let user = JSON.parse(loggedInUser);
+username.innerHTML = user.username;
+profilePicture.src = user.avatar;
 
-console.log(loggedInUser.username);
+console.log(user.avatar);

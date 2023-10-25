c09a8a06b61524ef4219c1dad011f00c74b2d94c
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-25 18:21:36 -0400

    Added basic implementation for adding "howls"

--- Homework4/static/home.js
+++ Homework4/static/home.js
@@ -4,9 +4,23 @@ import api from './APIClient.js';
 const loggedInUser = localStorage.getItem('user');
 const profilePicture = document.querySelector('#profilePic');
 const username = document.querySelector('#Username');
+const howlBox = document.querySelector('#howltextbox');
+const howlList = document.querySelector('#howlList');
 // username.innerHTML = loggedInUser.username;
 let user = JSON.parse(loggedInUser);
 username.innerHTML = user.username;
 profilePicture.src = user.avatar;
 
 console.log(user.avatar);
+
+
+
+const howlButton = document.querySelector('#HowlButton');
+howlButton.addEventListener('click', e => {
+    const message = document.createElement('h2');
+    message.innerHTML = howlBox.value;
+    message.className = "container-fluid text-center";
+    message.style.backgroundColor = "white";
+    howlList.append(message);
+
+});

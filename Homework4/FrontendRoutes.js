24002e5ccbd09b7f0ff6b51cd663a4313bf91be8
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-24 23:38:30 -0400

    Made the Login page and made it the default page

--- Homework4/FrontendRoutes.js
+++ Homework4/FrontendRoutes.js
@@ -5,7 +5,7 @@ const path = require('path');
 const html_dir = path.join(__dirname ,'/templates/');
 
 router.get('/', (req, res) => {
-  res.sendFile(`${html_dir}index.html`);
+  res.sendFile(`${html_dir}login.html`);
 });
 
 router.get('/login', (req, res) => {

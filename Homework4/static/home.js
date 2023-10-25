28a2b0bc7fc6bb6f031ba0e00f5c59ddd3c82bfb
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-25 01:07:25 -0400

    Added some things I'll need to change

--- Homework4/static/home.js
+++ Homework4/static/home.js
@@ -1,1 +1,7 @@
 import api from './APIClient.js';
+
+
+const loggedInUser = localStorage.getItem('loggedUser');
+const username = document.querySelector('#Username');
+username.innerHTML = loggedInUser.username;
+console.log(loggedInUser);
--- Homework4/static/login.js
+++ Homework4/static/login.js
@@ -4,7 +4,15 @@ const buttonSelect = document.querySelector('#submit');
 buttonSelect.addEventListener('click', e => {
   const usernameBox = document.querySelector('#usernameBox');
   console.log(usernameBox.value);
-  api.getUser(usernameBox.value);
-
+  api.getUser(usernameBox.value).then(user => {
+    console.log("From the server:", user);
+    let userDetails = {
+      username: user.username,
+      avatar: user.avatar,
+    }
+    localStorage.setItem('loggedUser', userDetails);
+    return user;
+  });
+  
   window.location.href = "/howler";
 });

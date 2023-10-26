b7f95d59d93b8b4680f2953d6221aea2451ad4aa
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-26 00:40:47 -0400

    Added times to the messages

--- Homework4/static/home.js
+++ Homework4/static/home.js
@@ -21,15 +21,19 @@ api.getFollows(user.username).then(followData => {
     alert("Not a Valid User.");
   });
   
-
+ 
   function getHowlsFromFollowers(array) {
+    let allHowls = [];
     for (let i = 0; i < array.length; i++) {
         api.getHowls(array[i]).then(howls => {
             
             
-
+            allHowls.push(howls);
+            
+            
             
             api.getUser(array[i]).then(followed => {
+                
                 for(let a = 0; a < howls.length; ++a) {
                     const howl = document.createElement('div');
                 howl.className = "container";
@@ -44,12 +48,19 @@ api.getFollows(user.username).then(followData => {
                 howl.style.backgroundColor = "orange";
                 howl.style.color = "white";
 
-                let currentdate = new Date(); 
-                let month = (currentdate.getMonth()+1) + "/";
-                let day = currentdate.getDate();
+                
+                let month = howls[a].datetime.substring(5, 7) + "/";
+                let day = howls[a].datetime.substring(8, 10);
                 day = day + ", ";
-                let hour = currentdate.getHours() + ":";
-                let minutes = currentdate.getMinutes() + "pm";
+                let hour = howls[a].datetime.substring(11, 13) + ":";
+                let minutes = ""; 
+                if (parseInt(hour) < 12) {
+                    minutes = howls[a].datetime.substring(14, 16) + "am";
+                }   
+                else {
+                    minutes = howls[a].datetime.substring(14, 16) + "pm";
+                }
+                
                 let postTime = month + day + hour + minutes;
 
                 const time = document.createElement('p');
@@ -67,12 +78,14 @@ api.getFollows(user.username).then(followData => {
                 howl.append(message);
                 
                 howlList.append(howl);
+                
                 }
               }).catch((err) => {
             
                 alert("Not a Valid User.");
               });
             
+              
 
             
         
@@ -81,10 +94,13 @@ api.getFollows(user.username).then(followData => {
             alert("Not a Valid User.");
           });
       }
-
-    //   console.log(allHowls);
+      console.log(allHowls);
   }
-  
+
+  function sorter (a, b) {
+    
+    return a.datetime < b.datetime ? -1 : 1;
+ };  
   
   
 const howlButton = document.querySelector('#HowlButton');
@@ -127,12 +143,3 @@ howlButton.addEventListener('click', e => {
 
 });
 
-const sorter = (a, b) => {
-    if(a.name === selfName){
-       return -1;
-    };
-    if(b.name === selfName){
-       return 1;
-    };
-    return a.name < b.name ? -1 : 1;
- };
